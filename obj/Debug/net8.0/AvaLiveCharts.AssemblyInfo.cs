7f9726bc34a58ce006b7a63384c6a34aeba4370f
RaLi <Rainer.Spam@posteo.de>
2024-11-24 12:52:36 +0100

    add .gitignore

--- obj/Debug/net8.0/AvaLiveCharts.AssemblyInfo.cs
+++ obj/Debug/net8.0/AvaLiveCharts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaLiveCharts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1b9c0edb82362f4955d15afa2eedb8b33ff686e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b226d2190382fc566d36ce0621b6816016bc878a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaLiveCharts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaLiveCharts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
